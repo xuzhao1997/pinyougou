16c52f3375d03884f4ab48b3c985fa2594dfd65f
xuzhao1997 <xuzhao123456>
2019-03-11 15:34:38 +0800

    分页封装类提交

---
+++ pinyougou_pojo/src/main/java/com/pinyougou/entity/PageResult.java
@@ -0,0 +1,12 @@
+package com.pinyougou.entity;
+
+import java.io.Serializable;
+
+/**
+ * @ClassName: 分页封装类
+ * @Description: java类作用描述
+ * @Author: XuZhao
+ * @CreateDate: 19/03/11$ 下午 03:33$
+ */
+public class PageResult implements Serializable {
+}
