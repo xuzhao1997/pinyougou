bd3ea93af195f27bd4c9153e7a5ba9519ca9b361
xuzhao1997 <xuzhao123456>
2019-03-12 21:05:16 +0800

    规格分页查询

---
+++ pinyougou_manager_web/src/main/webapp/js/service/specificationService.js
@@ -0,0 +1,32 @@
+//定义服务层
+app.service("specificationService",function ($http) {
+    //查询
+    this.findAll=function () {
+        return $http.get('../specification/findAll.do');
+    }
+    //分页查询
+    this.findPage=function (pageNum,pageSize) {
+        return $http.get("../specification/findPage.do?pageNum="+pageNum+"&pageSize="+pageSize);
+    }
+    //条件分页查询
+    this.search=function (pageNum,pageSize,searchEntity) {
+        return $http.post("../specification/search.do?pageNum="+pageNum+"&pageSize="+pageSize,searchEntity);
+    }
+    //根据id回显数据
+    this.findOne=function (id) {
+        return $http.get("../specification/findOne.do?id="+id);
+    }
+    //新增
+    this.add=function (entity) {
+        return $http.post("../specification/add.do",entity);
+    }
+    //修改
+    this.update=function (entity) {
+        return $http.post("../specification/update.do",entity);
+    }
+    //批量删除
+    this.dele=function (ids) {
+        return $http.get("../specification/delete.do?ids="+ids);
+    }
+
+})
