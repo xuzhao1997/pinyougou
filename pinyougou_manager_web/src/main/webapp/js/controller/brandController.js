afe5a7f4ecec6bba8c8eae3921166f61b953eaca
xuzhao1997 <xuzhao123456>
2019-03-12 16:38:35 +0800

    控件的优化

--- pinyougou_manager_web/src/main/webapp/js/controller/brandController.js
+++ pinyougou_manager_web/src/main/webapp/js/controller/brandController.js
@@ -6,19 +6,7 @@ app.controller('brandController',function ($scope,brandService) {
             }
         )
     };
-    //定义分页控件对象
-    $scope.paginationConf = {
-        currentPage:1,  				//当前页
-        totalItems:10,					//总记录数
-        itemsPerPage:10,				//每页记录数
-        perPageOptions:[10,20,30,40,50], //分页选项，下拉选择一页多少条记录
-        onChange:function(){			//页面变更后触发的方法
-            $scope.reloadList();		//启动就会调用分页组件
-        }
-    };
-    $scope.reloadList=function () {
-        $scope.findPage($scope.paginationConf.currentPage,$scope.paginationConf.itemsPerPage);
-    }
+
     //发起分页查询请求
     $scope.findPage=function (pageNum,pageSize) {
         brandService.findPage(pageNum,pageSize).success(function (response) {
@@ -54,17 +42,6 @@ app.controller('brandController',function ($scope,brandService) {
         )
     }
 
-    //定义一个批量删除的数组
-    $scope.selectIds=[];
-    //更新复选框选中状态的方法
-    $scope.updateSelection=function ($event,id) {
-        if($event.target.checked){
-            $scope.selectIds.push(id);
-        }else{
-            var index = $scope.selectIds.indexOf(id);
-            $scope.selectIds.splice(index,1);
-        }
-    }
     //批量删除数据
     $scope.dele=function () {
         if(confirm('您确定要删除吗?')){
