396b57455b0d8be30668b77e4cc652794298ed5b
xuzhao1997 <xuzhao123456>
2019-03-17 19:47:08 +0800

    组合实体类

---
+++ pinyougou_pojo/src/main/java/groupEntity/Goods.java
@@ -0,0 +1,45 @@
+package groupEntity;
+
+import com.pinyougou.pojo.TbGoods;
+import com.pinyougou.pojo.TbGoodsDesc;
+import com.pinyougou.pojo.TbItem;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @ClassName: Goods
+ * @Description: java类作用描述
+ * @Author: XuZhao
+ * @CreateDate: 19/03/17$ 下午 07:06$
+ */
+public class Goods implements Serializable {
+
+    private TbGoods goods;
+    private TbGoodsDesc goodsDesc;
+    private List<TbItem> itemList;
+
+    public TbGoods getGoods() {
+        return goods;
+    }
+
+    public void setGoods(TbGoods goods) {
+        this.goods = goods;
+    }
+
+    public TbGoodsDesc getGoodsDesc() {
+        return goodsDesc;
+    }
+
+    public void setGoodsDesc(TbGoodsDesc goodsDesc) {
+        this.goodsDesc = goodsDesc;
+    }
+
+    public List<TbItem> getItemList() {
+        return itemList;
+    }
+
+    public void setItemList(List<TbItem> itemList) {
+        this.itemList = itemList;
+    }
+}
