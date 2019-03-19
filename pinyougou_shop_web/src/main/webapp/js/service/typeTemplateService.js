67583a6c3720b4149f7011f8786c091bb745c955
xuzhao1997 <xuzhao123456>
2019-03-19 22:20:56 +0800

    规格查询

--- pinyougou_shop_web/src/main/webapp/js/service/typeTemplateService.js
+++ pinyougou_shop_web/src/main/webapp/js/service/typeTemplateService.js
@@ -17,7 +17,7 @@ app.service('typeTemplateService',function($http){
 	this.add=function(entity){
 		return  $http.post('../typeTemplate/add.do',entity );
 	}
-	//修改 
+	//修改
 	this.update=function(entity){
 		return  $http.post('../typeTemplate/update.do',entity );
 	}
@@ -29,5 +29,11 @@ app.service('typeTemplateService',function($http){
 	this.search=function(page,rows,searchEntity){
 		return $http.post('../typeTemplate/search.do?page='+page+"&rows="+rows, searchEntity);
 	}
+	//模板查询
+	this.selectSpecList=function (typeTemplateId) {
+		return $http.get("../typeTemplate/selectSpecList.do?typeTemplateId="+typeTemplateId);
+    }
+
+
 
 });
