7ced9d9d4ecdf9d0733b819189dba9753e853bb1
xuzhao1997 <xuzhao123456>
2019-03-19 22:20:09 +0800

    规格查询

--- pinyougou_sellergoods_service/src/main/java/com/pinyougou/sellergoods/service/impl/GoodsServiceImpl.java
+++ pinyougou_sellergoods_service/src/main/java/com/pinyougou/sellergoods/service/impl/GoodsServiceImpl.java
@@ -1,5 +1,9 @@
 package com.pinyougou.sellergoods.service.impl;
 import java.util.List;
+
+import com.pinyougou.mapper.TbGoodsDescMapper;
+import com.pinyougou.pojo.TbGoodsDesc;
+import groupEntity.Goods;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.github.pagehelper.Page;
@@ -22,6 +26,9 @@ public class GoodsServiceImpl implements GoodsService {
 
 	@Autowired
 	private TbGoodsMapper goodsMapper;
+
+	@Autowired
+    private TbGoodsDescMapper goodsDescMapper;
 	
 	/**
 	 * 查询全部
@@ -45,9 +52,17 @@ public class GoodsServiceImpl implements GoodsService {
 	 * 增加
 	 */
 	@Override
-	public void add(TbGoods goods) {
-		goodsMapper.insert(goods);		
-	}
+	public void add(Goods goods) {
+        //tb_goods表数据保存
+        TbGoods tbGoods = goods.getGoods();
+        tbGoods.setAuditStatus("0");//未审核状态
+        goodsMapper.insert(tbGoods);
+
+        //tb_goods_desc表数据保存
+        TbGoodsDesc goodsDesc = goods.getGoodsDesc();
+        goodsDesc.setGoodsId(tbGoods.getId());
+        goodsDescMapper.insert(goodsDesc);
+    }
 
 	
 	/**
