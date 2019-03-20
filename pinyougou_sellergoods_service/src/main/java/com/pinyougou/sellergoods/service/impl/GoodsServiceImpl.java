1a9db0a4234856c473de5281922e18270a9529b9
xuzhao1997 <xuzhao123456>
2019-03-20 20:25:37 +0800

    新增商品没有规格代码提交

--- pinyougou_sellergoods_service/src/main/java/com/pinyougou/sellergoods/service/impl/GoodsServiceImpl.java
+++ pinyougou_sellergoods_service/src/main/java/com/pinyougou/sellergoods/service/impl/GoodsServiceImpl.java
@@ -79,49 +79,88 @@ public class GoodsServiceImpl implements GoodsService {
         goodsDesc.setGoodsId(tbGoods.getId());
         goodsDescMapper.insert(goodsDesc);
 		//tb_item表数据保存\
-		List<TbItem> itemList = goods.getItemList();
-		for (TbItem tbItem : itemList) {
-			String title = tbGoods.getGoodsName();
-			//获取商品规格选项名称
-            String spec = tbItem.getSpec();
-            Map<String,String> map = JSON.parseObject(spec, Map.class);
-            for(String key:map.keySet()){
-                title+=" "+map.get(key);
+
+        //启用规格
+        if(tbGoods.getIsEnableSpec().equals("1")){//启用规格
+            //tb_item表数据保存
+            List<TbItem> itemList = goods.getItemList();
+            for (TbItem item : itemList) {
+//		  `title` varchar(100) NOT NULL COMMENT '商品标题',   // 商品名称（SPU名称）+ 商品规格选项名称 中间以空格隔开
+                String title=tbGoods.getGoodsName();
+                //获取商品规格选项名称
+                //:{"网络":"移动3G","机身内存":"32G"}
+                String spec = item.getSpec();
+                Map<String,String> map = JSON.parseObject(spec, Map.class);
+                for(String key : map.keySet()){
+                    title+=" "+ map.get(key);
+                }
+                item.setTitle(title);
+                //封装tb_item后端需要组装的数据
+                setItemValue(tbGoods, goodsDesc, item);
+
+                itemMapper.insert(item);
             }
-            tbItem.setTitle(title);
-            //取图片
-            String itemImages = goodsDesc.getItemImages();
-            List<Map> imagesList = JSON.parseArray(itemImages, Map.class);
-            if(imagesList!=null && imagesList.size()>0){
-                String image = (String) imagesList.get(0).get("url");
-                tbItem.setImage(image);
-            }
-            //三级分类
-            tbItem.setCategoryid(tbGoods.getCategory3Id());
-            //创建时间
-            tbItem.setCreateTime(new Date());
-            //更新时间
-            tbItem.setUpdateTime(new Date());
-            //goods_id
-            tbItem.setGoodsId(tbGoods.getId());
-            //seller_id
-            tbItem.setSellerId(tbGoods.getSellerId());
-            //三级分类名称
-            String category = itemCatMapper.selectByPrimaryKey(tbGoods.getCategory3Id()).getName();
-            tbItem.setCategory(category);
-            //品牌名称
-            String brand = brandMapper.selectByPrimaryKey(tbGoods.getBrandId()).getName();
-            tbItem.setBrand(brand);
-            //商家店铺名称
-            String seller = sellerMapper.selectByPrimaryKey(tbGoods.getSellerId()).getNickName();
-            tbItem.setSeller(seller);
-            itemMapper.insert(tbItem);
+        }else {//没有启用规格
+            //需要后端自己组装sku（item）的相关数据
+            TbItem item = new TbItem();
+            //spu名称作为title
+            String title=tbGoods.getGoodsName();
+            item.setTitle(title);
+
+            //封装tb_item后端需要组装的数据
+            setItemValue(tbGoods, goodsDesc, item);
+
+            //封装原本由页面提交的数据
+            item.setSpec("{}");
+            item.setPrice(tbGoods.getPrice());
+            item.setNum(999);
+            item.setStatus("1");
+            item.setIsDefault("1");
+            itemMapper.insert(item);
         }
+
 
     }
 
-	
-	/**
+    /**
+    * @Description: 封装tb_item表后端需要组装的数据
+    * @Author:      XuZhao
+    * @CreateDate:  19/03/20 下午 07:43
+    */
+    private void setItemValue(TbGoods tbGoods, TbGoodsDesc goodsDesc, TbItem item) {
+        //		  `image` varchar(2000) DEFAULT NULL COMMENT '商品图片',  // 从 tb_goods_desc item_images中获取第一张
+        //[{"color":"白色","url":"http://192.168.25.133/group1/M00/00/00/wKgZhVnGbYuAO6AHAAjlKdWCzvg253.jpg"},
+        // {"color":"黑色","url":"http://192.168.25.133/group1/M00/00/00/wKgZhVnKX5WAOsqXAAETwD7A1Is409.jpg"}]
+        String itemImages = goodsDesc.getItemImages();
+        List<Map> imageList = JSON.parseArray(itemImages, Map.class);
+        if (imageList!=null && imageList.size()>0) {
+            String image= (String) imageList.get(0).get("url");
+            item.setImage(image);
+        }
+//		  `categoryId` bigint(10) NOT NULL COMMENT '所属类目，叶子类目',  //三级分类id
+        item.setCategoryid(tbGoods.getCategory3Id());
+//		  `create_time` datetime NOT NULL COMMENT '创建时间',
+        item.setCreateTime(new Date());
+//		  `update_time` datetime NOT NULL COMMENT '更新时间',
+        item.setUpdateTime(new Date());
+//		  `goods_id` bigint(20) DEFAULT NULL,
+        item.setGoodsId(tbGoods.getId());
+//		  `seller_id` varchar(30) DEFAULT NULL,
+        item.setSellerId(tbGoods.getSellerId());
+//				//以下字段作用：方便商品搜索
+//		  `category` varchar(200) DEFAULT NULL, //三级分类名称
+        String category = itemCatMapper.selectByPrimaryKey(tbGoods.getCategory3Id()).getName();
+        item.setCategory(category);
+//		  `brand` varchar(100) DEFAULT NULL,//品牌名称
+        String brandName = brandMapper.selectByPrimaryKey(tbGoods.getBrandId()).getName();
+        item.setBrand(brandName);
+//		  `seller` varchar(200) DEFAULT NULL,//商家店铺名称
+        String nickName = sellerMapper.selectByPrimaryKey(tbGoods.getSellerId()).getNickName();
+        item.setSeller(nickName);
+    }
+
+
+    /**
 	 * 修改
 	 */
 	@Override
