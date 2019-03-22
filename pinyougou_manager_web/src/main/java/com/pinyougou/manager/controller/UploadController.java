e3e1a34901a4db55c1417dfad34bdfb4fc8a0f04
xuzhao1997 <xuzhao123456>
2019-03-22 20:48:56 +0800

    门户网站雏形搭建

--- pinyougou_manager_web/src/main/java/com/pinyougou/manager/controller/UploadController.java
+++ pinyougou_manager_web/src/main/java/com/pinyougou/manager/controller/UploadController.java
@@ -7,41 +7,34 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import util.FastDFSClient;
 
-/**
- * @ClassName: UploadController
- * @Description: 文件上传表现层
- * @Author: XuZhao
- * @CreateDate: 19/03/19$ 下午 04:55$
- */
 @RestController
 @RequestMapping("/upload")
 public class UploadController {
 
+    //获取properties配置文件的内容 spel
     @Value("${FILE_SERVER_URL}")
     private String FILE_SERVER_URL;
 
     /**
-    * @Description: 接收页面提交的文件对象,完成文件上传
-    * @Author:      XuZhao
-    * @CreateDate:  19/03/19 下午 04:56
-    */
+     * 接收页面提交的文件对象，完成文件上传操作
+     */
     @RequestMapping("/uploadFile")
     public Result uploadFile(MultipartFile file){
         try {
-            //获取文件原名称
+            //获取文件源名称 "1.jpg"
             String originalFilename = file.getOriginalFilename();
-            String extName = originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
+            String extName =originalFilename.substring(originalFilename.lastIndexOf(".")+1);
             //基于FastDFS工具类实现文件上传操作
             FastDFSClient fastDFSClient = new FastDFSClient("classpath:config/fdfs_client.conf");
-
+            //参数一：文件对象内容 参数二：文件扩展名
+            //path=/M00/00/00/wKgZhVyQVOGAEbRoAABanUWeaGY033.jpg
             String path = fastDFSClient.uploadFile(file.getBytes(), extName);
             System.out.println(path);
-            String url = FILE_SERVER_URL+path;
+            String url =FILE_SERVER_URL+path;
             return new Result(true,url);
         } catch (Exception e) {
             e.printStackTrace();
-            return new Result(false,"上传失败!!");
+            return new Result(false,"上传失败");
         }
     }
-
 }
