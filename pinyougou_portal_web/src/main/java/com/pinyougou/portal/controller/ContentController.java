73d9d5c668336c2fa15cb2d535b037e5a2d808df
xuzhao1997 <xuzhao123456>
2019-03-22 22:32:18 +0800

    前台网站广告轮播图展示

---
+++ pinyougou_portal_web/src/main/java/com/pinyougou/portal/controller/ContentController.java
@@ -0,0 +1,37 @@
+package com.pinyougou.portal.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.content.service.ContentService;
+import com.pinyougou.pojo.TbContent;
+import entity.PageResult;
+import entity.Result;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * controller
+ * @author Administrator
+ *
+ */
+@RestController
+@RequestMapping("/content")
+public class ContentController {
+
+	@Reference
+	private ContentService contentService;
+	
+
+	/**
+	* @Description: 根据广告分类,查询相关广告列表数据
+	* @Author:      XuZhao
+	* @CreateDate:  19/03/22 下午 08:51
+	*/
+	@RequestMapping("/findByCategoryId")
+	public List<TbContent> findByCategoryId(Long categoryId){
+		return contentService.findByCategoryId(categoryId);
+	}
+	
+}
