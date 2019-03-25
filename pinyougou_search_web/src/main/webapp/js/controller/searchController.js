2af70b3704a1b91954f484831b80dcb63916a6bb
xuzhao1997 <xuzhao123456>
2019-03-25 21:05:09 +0800

    搜索查询之条件搜索

--- pinyougou_search_web/src/main/webapp/js/controller/searchController.js
+++ pinyougou_search_web/src/main/webapp/js/controller/searchController.js
@@ -5,7 +5,15 @@ app.controller("searchController",function ($scope,$controller,searchService) {
 
     //初始化搜索条件对象
     $scope.searchMap={
-        keywords:""
+        keywords:"",
+        category:"",//分类条件
+        brand:"",
+        spec:{},//规格条件
+        price:"",
+        sortField:"",//排序字段
+        sort:"ASC",//排序方式
+        pageNum:1,
+        pageSize:60
     };
     //商品搜索功能
     $scope.search=function () {
@@ -14,4 +22,38 @@ app.controller("searchController",function ($scope,$controller,searchService) {
         })
     }
 
+    //条件过滤查询
+    $scope.addFilterCondition=function (key,value) {
+        //判断条件如果是品牌,分类,价格区间
+        if(key=="brand" || key=="category" || key=="price"){
+            $scope.searchMap[key]=value;
+        }else{
+            //规格条件,组装$scope.searchMap规格名称和对应的选项值
+            $scope.searchMap.spec[key]=value;
+        }
+        //从新查询商品数据
+        $scope.search($scope.searchMap);
+    }
+
+    //删除条件过滤查询
+    $scope.removeSearchItem=function (key) {
+        //判断条件如果是品牌,分类,价格区间
+        if(key=="brand" || key=="category" || key=="price"){
+            $scope.searchMap[key]="";
+        }else{
+            //规格条件,组装$scope.searchMap规格名称和对应的选项值
+           delete $scope.searchMap.spec[key]
+        }
+        //从新查询商品数据
+        $scope.search($scope.searchMap);
+    }
+    
+    //排序查询
+    $scope.sortSearch=function (sortField,sort) {
+        $scope.searchMap.sort = sort;//组装排序方式
+        $scope.searchMap.sortField = sortField;//组装排序字段
+        //从新查询商品数据
+        $scope.search($scope.searchMap);
+    }
+    
 })
