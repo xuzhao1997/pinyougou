2e346c4e25e2aac590de9d525900e2c3491133d8
xuzhao1997 <xuzhao123456>
2019-03-26 19:33:19 +0800

    商品详情分类ID展示

--- pinyougou_page_service/src/main/java/com/pinyougou/page/service/impl/pageServiceImpl.java
+++ pinyougou_page_service/src/main/java/com/pinyougou/page/service/impl/pageServiceImpl.java
@@ -3,6 +3,7 @@ package com.pinyougou.page.service.impl;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.pinyougou.mapper.TbGoodsDescMapper;
 import com.pinyougou.mapper.TbGoodsMapper;
+import com.pinyougou.mapper.TbItemCatMapper;
 import com.pinyougou.mapper.TbItemMapper;
 import com.pinyougou.page.service.pageService;
 import com.pinyougou.pojo.TbGoods;
@@ -13,7 +14,9 @@ import groupEntity.Goods;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @ClassName: 类名
@@ -34,6 +37,9 @@ public class pageServiceImpl implements pageService {
     @Autowired
     private TbItemMapper itemMapper;
 
+    @Autowired
+    private TbItemCatMapper itemCatMapper;
+
 
    /**
    * @Description: 组装静态页生成所需要的参数
@@ -49,6 +55,20 @@ public class pageServiceImpl implements pageService {
         //tb_goods_desc相关数据
         TbGoodsDesc tbGoodsDesc = goodsDescMapper.selectByPrimaryKey(goodsId);
         goods.setGoodsDesc(tbGoodsDesc);
+        //组装商品分类数据
+        //一级分类id
+        String category1Name = itemCatMapper.selectByPrimaryKey(tbGoods.getCategory1Id()).getName();
+        //二级
+        String category2Name = itemCatMapper.selectByPrimaryKey(tbGoods.getCategory2Id()).getName();
+        //三级
+        String category3Name = itemCatMapper.selectByPrimaryKey(tbGoods.getCategory3Id()).getName();
+        //组装分类列表数据
+        Map<String,String> categoryMap = new HashMap<>();
+        categoryMap.put("category1Name",category1Name);
+        categoryMap.put("category2Name",category2Name);
+        categoryMap.put("category3Name",category3Name);
+        goods.setCategoryMap(categoryMap);
+
         //tb_item列表相关数据
         TbItemExample example = new TbItemExample();
         TbItemExample.Criteria criteria = example.createCriteria();
