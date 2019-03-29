b3902d4fd7aafe89b9119eb00320db79b40f294c
xuzhao1997 <xuzhao123456>
2019-03-29 22:04:11 +0800

    用户注册之短信验证码发送验证

--- pinyougou_user_web/src/main/webapp/js/service/userService.js
+++ pinyougou_user_web/src/main/webapp/js/service/userService.js
@@ -14,8 +14,8 @@ app.service('userService',function($http){
 		return $http.get('user/findOne.do?id='+id);
 	}
 	//增加 
-	this.add=function(entity){
-		return  $http.post('user/add.do',entity );
+	this.add=function(entity,smsCode){
+		return  $http.post('user/add.do?smsCode='+smsCode,entity );
 	}
 	//修改 
 	this.update=function(entity){
