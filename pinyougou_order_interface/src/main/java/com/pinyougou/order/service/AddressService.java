1430b71b74b91b75b1953b9836f469169059cd5d
xuzhao1997 <xuzhao123456>
2019-04-03 22:13:21 +0800

    订单模块提交接口

---
+++ pinyougou_order_interface/src/main/java/com/pinyougou/order/service/AddressService.java
@@ -0,0 +1,67 @@
+package com.pinyougou.order.service;
+import java.util.List;
+import com.pinyougou.pojo.TbAddress;
+
+import entity.PageResult;
+/**
+ * 服务层接口
+ * @author Administrator
+ *
+ */
+public interface AddressService {
+
+	/**
+	 * 返回全部列表
+	 * @return
+	 */
+	public List<TbAddress> findAll();
+	
+	
+	/**
+	 * 返回分页列表
+	 * @return
+	 */
+	public PageResult findPage(int pageNum, int pageSize);
+	
+	
+	/**
+	 * 增加
+	*/
+	public void add(TbAddress address);
+	
+	
+	/**
+	 * 修改
+	 */
+	public void update(TbAddress address);
+	
+
+	/**
+	 * 根据ID获取实体
+	 * @param id
+	 * @return
+	 */
+	public TbAddress findOne(Long id);
+	
+	
+	/**
+	 * 批量删除
+	 * @param ids
+	 */
+	public void delete(Long[] ids);
+
+	/**
+	 * 分页
+	 * @param pageNum 当前页 码
+	 * @param pageSize 每页记录数
+	 * @return
+	 */
+	public PageResult findPage(TbAddress address, int pageNum, int pageSize);
+
+	/**
+	* @Description: 根据登录人,查询该用户关联的收件人地址列表
+	* @Author:      XuZhao
+	* @CreateDate:  19/04/03 下午 07:19
+	*/
+	List<TbAddress> findByUserId(String userId);
+}
