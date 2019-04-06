ceeb92b5018deab08ad188ac0078e4b3e76a8728
xuzhao1997 <xuzhao123456>
2019-04-06 20:00:10 +0800

    微信支付成功

--- pinyougou_order_service/src/main/java/com/pinyougou/order/service/impl/OrderServiceImpl.java
+++ pinyougou_order_service/src/main/java/com/pinyougou/order/service/impl/OrderServiceImpl.java
@@ -1,11 +1,14 @@
 package com.pinyougou.order.service.impl;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import com.pinyougou.mapper.TbOrderItemMapper;
+import com.pinyougou.mapper.TbPayLogMapper;
 import com.pinyougou.order.service.OrderService;
 import com.pinyougou.pojo.TbOrderItem;
+import com.pinyougou.pojo.TbPayLog;
 import groupEntity.Cart;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.alibaba.dubbo.config.annotation.Service;
@@ -41,6 +44,10 @@ public class OrderServiceImpl implements OrderService {
 
 	@Autowired
 	private TbOrderItemMapper orderItemMapper;
+
+	@Autowired
+	private TbPayLogMapper payLogMapper;
+
 	
 	/**
 	 * 查询全部
@@ -68,10 +75,17 @@ public class OrderServiceImpl implements OrderService {
 		//从购物车中,基于存在几个商家,生成几个订单
 		String userId = order.getUserId();
 		List<Cart> cartList = (List<Cart>) redisTemplate.boundValueOps(userId).get();
+		//支付总金额
+		Double totalPayment = 0.00;
+		//定义记录订单id的集合
+		List<String> ids = new ArrayList<>();
+
 		for (Cart cart : cartList) {
 			//生成订单
 			TbOrder tbOrder = new TbOrder();
 			long ordeId = idWorker.nextId();
+			//组装支付关联的Id集合数据
+			ids.add(ordeId+"");
 			tbOrder.setOrderId(ordeId);
 			//新生成的订单全部设置为未支付状态
 			tbOrder.setStatus("1");
@@ -95,11 +109,35 @@ public class OrderServiceImpl implements OrderService {
 				payment = orderItem.getTotalFee().doubleValue();
 				orderItemMapper.insert(orderItem);
 			}
+			//计算支付总金额,每笔订单支付金额累加即支付总金额
+			totalPayment+=payment;
 			//订单支付金额,该订单中每个商品的小计金额相加即可
 			tbOrder.setPayment(new BigDecimal(payment));
 
 			orderMapper.insert(tbOrder);
 		}
+		//如果支付方式采用在线支付,需要在系统中针对该订单记录一笔支付操作
+		if("1".equals(order.getPaymentType())){
+			TbPayLog payLog = new TbPayLog();
+
+			payLog.setOutTradeNo(idWorker.nextId()+"");
+			payLog.setCreateTime(new Date());
+			payLog.setTotalFee((long) (totalPayment*100));
+			payLog.setUserId(userId);
+			payLog.setTradeState("1");
+			payLog.setOrderList(ids.toString().replace("[","").replace("]","").replace(" ",""));
+			payLog.setPayType("1");//微信支付 1
+			//支付类型
+			payLog.setPayType("1");
+			//保存支付日志
+			payLogMapper.insert(payLog);
+			//将支付日志保存到缓存中
+			redisTemplate.boundHashOps("payLog").put(userId,payLog);
+		}
+
+
+		//保存订单后,清除redis购物车列表数据
+		redisTemplate.delete(userId);
 
 	}
 
