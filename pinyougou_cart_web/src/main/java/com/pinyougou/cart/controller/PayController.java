8d5872a08d91ed2e203bfec0b9f1a8d3e8275b8e
xuzhao1997 <xuzhao123456>
2019-04-06 17:34:49 +0800

    微信支付成功跳转与失败

--- pinyougou_cart_web/src/main/java/com/pinyougou/cart/controller/PayController.java
+++ pinyougou_cart_web/src/main/java/com/pinyougou/cart/controller/PayController.java
@@ -2,6 +2,7 @@ package com.pinyougou.cart.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pay.service.PayService;
+import entity.Result;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import util.IdWorker;
@@ -38,7 +39,40 @@ public class PayController {
             e.printStackTrace();
             return new HashMap<>();
         }
-
     }
+
+    /**
+    * @Description: 查询支付状态
+    * @Author:      XuZhao
+    * @CreateDate:  19/04/06 下午 04:44
+    */
+    @RequestMapping("/queryPayStatus")
+    public Result queryPayStatus(String out_trade_no){
+        try {
+            int count=1;
+            while (true){
+                //每隔三秒调一次
+                Thread.sleep(3000);
+
+                //跳出循环的时机,设置二维码失效.
+                count++;
+                if(count>100){
+                    return new Result(false,"timeout");
+                }
+
+                //查询结果集合参数
+                Map<String, String> resultMap = payService.queryPayStatus(out_trade_no);
+                //获取支付状态
+                String trade_state = resultMap.get("trade_state");
+                if("SUCCESS".equals(trade_state)){
+                    return new Result(true,"支付成功!!!");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false,"支付失败!!!");
+        }
+    }
+
 
 }
