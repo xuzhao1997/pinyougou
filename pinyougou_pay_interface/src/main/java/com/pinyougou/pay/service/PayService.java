b19a53cb39760afecf538c7a335e0fb2076c5652
xuzhao1997 <xuzhao123456>
2019-04-06 17:34:19 +0800

    微信支付成功跳转与失败

--- pinyougou_pay_interface/src/main/java/com/pinyougou/pay/service/PayService.java
+++ pinyougou_pay_interface/src/main/java/com/pinyougou/pay/service/PayService.java
@@ -18,5 +18,11 @@ public interface PayService {
     */
     public Map<String,Object> createNative(String out_trade_no,String total_fee) throws Exception;
 
+    /**
+    * @Description: 查询微信支付状态
+    * @Author:      XuZhao
+    * @CreateDate:  19/04/06 下午 04:32
+    */
 
+    public Map<String,String> queryPayStatus(String out_trade_no) throws Exception;
 }
