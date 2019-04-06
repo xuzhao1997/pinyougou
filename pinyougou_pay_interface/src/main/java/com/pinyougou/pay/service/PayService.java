3029b8705c86ea2fc1b7a0d96562ea984fa11828
xuzhao1997 <xuzhao123456>
2019-04-06 19:59:53 +0800

    微信支付日志

--- pinyougou_pay_interface/src/main/java/com/pinyougou/pay/service/PayService.java
+++ pinyougou_pay_interface/src/main/java/com/pinyougou/pay/service/PayService.java
@@ -1,5 +1,7 @@
 package com.pinyougou.pay.service;
 
+import com.pinyougou.pojo.TbPayLog;
+
 import java.util.Map;
 
 /**
@@ -25,4 +27,11 @@ public interface PayService {
     */
 
     public Map<String,String> queryPayStatus(String out_trade_no) throws Exception;
+
+    /**
+    * @Description: 获取支付订单号,和支付金额,
+    * @Author:      XuZhao
+    * @CreateDate:  19/04/06 下午 07:45
+    */
+    TbPayLog getPayLog(String userId);
 }
