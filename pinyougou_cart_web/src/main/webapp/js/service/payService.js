9cb17b1ed41bdbef957449b0134d8b98441c9a46
xuzhao1997 <xuzhao123456>
2019-04-06 17:34:27 +0800

    微信支付成功跳转与失败

--- pinyougou_cart_web/src/main/webapp/js/service/payService.js
+++ pinyougou_cart_web/src/main/webapp/js/service/payService.js
@@ -6,4 +6,9 @@ app.service('payService',function($http){
 		return $http.get('pay/createNative.do');
 	}
 
+    //查询支付状态
+    this.queryPayStatus=function(out_trade_no){
+        return $http.get('pay/queryPayStatus.do?out_trade_no='+out_trade_no);
+    }
+
 });
