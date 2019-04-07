0e4313cfc293e4e39062dfa017a5bd47e1123069
xuzhao1997 <xuzhao123456>
2019-04-07 16:35:51 +0800

    秒杀商品详情展示

--- pinyougou_seckill_web/src/main/webapp/js/controller/seckillController.js
+++ pinyougou_seckill_web/src/main/webapp/js/controller/seckillController.js
@@ -9,5 +9,13 @@ app.controller('seckillController' ,function($scope,$controller   ,$location,sec
             $scope.seckillGoodsList=response;
         })
     }
+    //基于秒杀商品id查询秒杀商品详情
+    $scope.findOne=function () {
+        //获取秒杀商品首页传递到秒杀详情页
+        $scope.seckillGoodsId = $location.search()["seckillGoodsId"];
+        seckillService.findOne($scope.seckillGoodsId).success(function (response) {
+            $scope.seckillGoods=response;
+        })
+    }
 
 });
