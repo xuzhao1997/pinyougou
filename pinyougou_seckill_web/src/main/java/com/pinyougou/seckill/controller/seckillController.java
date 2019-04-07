262fec085bc760c417b60f0086330945e0bce046
xuzhao1997 <xuzhao123456>
2019-04-07 19:08:12 +0800

    秒杀商品订单实现

--- pinyougou_seckill_web/src/main/java/com/pinyougou/seckill/controller/seckillController.java
+++ pinyougou_seckill_web/src/main/java/com/pinyougou/seckill/controller/seckillController.java
@@ -3,6 +3,8 @@ package com.pinyougou.seckill.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pojo.TbSeckillGoods;
 import com.pinyougou.seckill.service.SeckillService;
+import entity.Result;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,5 +42,26 @@ public class seckillController {
     @RequestMapping("/findOne")
     public TbSeckillGoods findOne(Long seckillGoodsId){
         return seckillService.findOne(seckillGoodsId);
+    }
+
+    /**
+    * @Description: 保存秒杀订单
+    * @Author:      XuZhao
+    * @CreateDate:  19/04/07 下午 04:59
+    */
+    @RequestMapping("/submitSeckillOrder")
+    public Result submitSeckillOrder(Long seckillGoodsId){
+        try {
+            //获取登录用户
+            String userId = SecurityContextHolder.getContext().getAuthentication().getName();
+            if(userId.equals("anonymousUser")){
+                return new Result(false,"抢购前，请先登录");
+            }
+            seckillService.submitSeckillOrder(seckillGoodsId,userId);
+            return new Result(true,"保存订单成功!!!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false,"保存订单失败!!!");
+        }
     }
 }
